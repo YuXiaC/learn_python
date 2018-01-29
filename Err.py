65d7c07c8d8d1e69bb0ea6014c67ad84d283710a
xgs <luguowo@foxmail.com>
2018-01-29 21:59:03 +0800

    test unit

---
+++ Err.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pdb
+import logging
+#logging.basicConfig(level=logging.INFO)
+import unittest
+
+
+"""
+try:
+    print 'try...'
+    r = 10 / 0
+    print 'result:', r
+except ZeroDivisionError, e:
+    print 'except:', e
+finally:
+    print 'finally...'
+print 'END'
+"""
+
+
+"""
+try:
+    print 'try...'
+    r = 10 / int('a')#不能这些写
+    print 'result:', r
+except ValueError, e:
+    print 'ValueError:', e
+except ZeroDivisionError, e:
+    print 'ZeroDivisionError:', e
+finally:
+    print 'finally...'
+print 'END'
+"""
+
+
+"""
+try:
+    print 'try...'
+    r = 10 / 5
+    print 'result:', r
+except ValueError, e:
+    print 'ValueError:', e
+except ZeroDivisionError, e:
+    print 'ZeroDivisionError:', e
+else:
+    print 'no error!'
+finally:
+    print 'finally...'
+print 'END'
+"""
+
+
+"""
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    try:
+        bar('0')
+    except StandardError, e:
+        print 'Error!'
+    finally:
+        print 'finally...'
+
+if __name__ == '__main__':
+    main()
+print "test"
+"""
+
+"""
+def foo(s):
+    n = int(s)
+    assert n != 0,"n is zero"#抛出的错误信息
+    return 10 / n
+
+def main():
+    foo('0')
+
+if __name__ == '__main__':
+    main()
+"""
+
+
+
+"""
+
+
+s = '0'
+n = int(s)
+logging.info('n = %d' % n)
+print 10 / n
+"""
+
+
+
+
+"""
+s = '0'
+n = int(s)
+pdb.set_trace() # 运行到这里会自动暂停
+print 10 / n
+"""
+
+
+#unit test
+class Dict(dict):
+
+    def __init__(self, **kw):
+        super(Dict, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+class TestDict(unittest.TestCase):
+
+    def test_init(self):
+        d = Dict(a=1, b='test')
+        self.assertEquals(d.a, 1)
+        self.assertEquals(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEquals(d.key, 'value')
+
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEquals(d['key'], 'value')
+
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+class Dict(dict):
+    '''
+    Simple dict but also support access as x.y style.
+
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    '''
+    def __init__(self, **kw):
+        super(Dict, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
