f1dd22f262735197594725668f17f77068768f0f
xgs <luguowo@foxmail.com>
2018-01-29 16:14:38 +0800

    amend Obj

--- Obj.py
+++ Obj.py
@@ -28,3 +28,30 @@ print bart.get_grade()
 # 也就是说，对于两个实例变量，虽然它们都是同一个类的不同实例，但拥有的变量名称都可能不同：
 bart.age=8
 print bart.age
+
+
+class Animal(object):
+    def run(self):
+        print 'Animal is running...'
+
+#继承
+class Dog(Animal):
+    def run(self):
+        print 'Dog is running...'
+
+class Cat(Animal):
+    def run(self):
+        print 'Cat is running...'
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+class Tortoise(Animal):
+    def run(self):
+        print 'Tortoise is running slowly...'
+
+run_twice(Animal())
+run_twice(Dog())
+run_twice(Cat())
+run_twice(Tortoise())
