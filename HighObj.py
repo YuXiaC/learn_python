85e845072ba19de0dfbe9b7b26e94a76abcc0f44
xgs <luguowo@foxmail.com>
2018-01-29 18:47:55 +0800

    amend HighObj

--- HighObj.py
+++ HighObj.py
@@ -104,4 +104,107 @@ print s.age
 """
 
 #多重继承
+"""
+class Animal(object):
+    pass
+
+# 大类:
+class Mammal(Animal):
+    pass
+
+class Bird(Animal):
+    pass
+
+class Runnable(object):
+    def run(self):
+        print('Running...')
+
+class Flyable(object):
+    def fly(self):
+        print('Flying...')
+
+
+# 各种动物:
+class Dog(Mammal,Runnable):
+    pass
+
+class Bat(Mammal,Flyable):
+    pass
+
+class Parrot(Bird):
+    pass
+
+class Ostrich(Bird):
+    pass
+"""
+
+#定制类
+class Student(object):
+    def __init__(self,name):
+        self.name=name
+    def __str__(self):
+        return "Student object (name:%s)"%self.name
+
+    #为调试服务
+    __repr__=__str__
+
+print Student("Michal")
+
+
+class Fib(object):
+    def __init__(self):
+        self.a,self.b=0,1
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        self.a,self.b=self.b,self.a+self.b
+        if self.a>1000000:
+            raise StopIteration()
+        return self.a
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            a,b=1,1
+            for x in range(item):
+                a,b=b,a+b
+            return a
+        if isinstance(item,slice):
+            start=item.start
+            stop=item.stop
+            a,b=1,1
+            L=[]
+            for x in range(stop):
+                if x>=start:
+                    L.append(a)
+                a,b=b,a+b
+            return L
+
+for n in Fib():
+    print n,
+print
+
+print Fib()[5]
+
+f=Fib()
+
+print f[:10]
+
+
+class Chain(object):
+    def __init__(self, path=''):
+        self._path = path
+
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+
+    def __str__(self):
+        return self._path
+
+    def __call__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+
+print Chain().status.user.timeline.list
+print Chain().users('michael').repos
 
